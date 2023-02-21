166f750cecb122d74cebd1721097bdea3d57a05e
Andrew Moss <1043609+amoss@users.noreply.github.com>
2023-02-21 17:06:36 +0100

    Expanded the constant expressions in the target grammar
    * Added the other primitive types with full bracket weirdness.
    * Checked that the generator works.
    * No discard channel for whitespace yet - will need a way to
      exclude it from between the terminals making up the unicode
      expressions otherwise leading/trailing whitespace will produce
      ambiguity.

--- tests/pidgin_const1/bootstrapper.py
+++ tests/pidgin_const1/bootstrapper.py
@@ -26,11 +26,40 @@ def build():
     for p in anyPrefixOf("unicode"):
         for l,r in brackets:
             const.add(         [g.Terminal(p), g.Terminal(l), g.Terminal(set([l,r]), "any", inverse=True), g.Terminal(r)])
+
+    for p in anyPrefixOf("order"):
+        for l,r in brackets:
+            const.add(         [g.Terminal(p), g.Terminal(l), g.Nonterminal("const", "any"), g.Terminal(r)])
+
+    for p in anyPrefixOf("set"):
+        for l,r in brackets:
+            const.add(         [g.Terminal(p), g.Terminal(l), g.Nonterminal("const", "any"), g.Terminal(r)])
+
+    for p in anyPrefixOf("map"):
+        for l,r in brackets:
+            const.add(         [g.Terminal(p), g.Terminal(l), g.Nonterminal("const_kv", "any"), g.Terminal(r)])
+
+    const_kv = g.addRule("const_kv", [g.Nonterminal("const"), g.Terminal(":"), g.Nonterminal("const")])
+
     graph = g.build()
     return g, graph
 
+# The spot for manual testing of the parser
+# if __name__=="__main__":
+#    grammar, graph = build()
+#    from bootstrap.parser2 import Parser
+#    parser = Parser(graph)
+#    print(list(parser.parse("unicode[öäå123]")))
+
+# The spot for manual testing of the generator
 if __name__=="__main__":
+    import itertools
+    from bootstrap.generator2 import Generator
     grammar, graph = build()
-    from bootstrap.parser2 import Parser
-    parser = Parser(graph)
-    print(list(parser.parse("unicode[öäå123]")))
+    generator = Generator(grammar)
+    sentences = list(itertools.islice(generator.step(), 2000))
+    with open("generated.txt","wt") as outputFile:
+        for s in sentences:
+            s = [ symb.string if symb.string is not None else list(symb.chars)[0] for symb in s]
+            outputFile.write("".join(s))
+            outputFile.write("\n")
