7b79117cd5eb8c9e64d8f4b62a2d26baff9e190d
Andrew Moss <1043609+amoss@users.noreply.github.com>
2023-02-21 08:39:29 +0100

    Add a simpler example of ambiguity
    * This is a reduced version of the problem that showed up in the incorrect
      version of toy brackets 3.
    * Simple sequence of terminals that can matched/generated in two distinct
      ways: either via iteration or recursion.
    * This demonstrates that the state merging is not working in the parser and
      thus could lead to an exponential blow-up in the size of the search space.

---
+++ tests/toy_ambiguous1/bootstrapper.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2003 Dr Andrew Moss.    You should have received a copy of the GNU General Public License
+#                                       along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os, sys
+rootDir= os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+if rootDir not in sys.path:
+    sys.path.append(rootDir)
+
+from bootstrap.graph import Graph
+from bootstrap.grammar import Grammar
+
+
+def build():
+    '''
+    Simple way to provoke ambiguity - list of xs can reduced by iteration or recursion.
+    lst <- x | x lst*
+    '''
+    g = Grammar("lst")
+    lst = g.addRule("lst", [g.Terminal("x")])
+    lst.add(               [g.Terminal("x"), g.Nonterminal("lst","any")])
+
+    graph = g.build()
+    return g, graph
