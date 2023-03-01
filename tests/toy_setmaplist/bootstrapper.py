d4d6d21531aa0166f4b8b9f53c7c106232790af8
Andrew Moss <1043609+amoss@users.noreply.github.com>
2023-03-01 14:40:12 +0100

    Final look at the expr_kv expr_lst subtrees in the trace for now
    * Added a small enough fragment of the expr2 grammar to allow the lr(0)
      rendering to be readable.
    * The test case shows that we do not admit invalid sentence as the
      handle check won't reduce { expr_kv expr_lst expr_kv}.
    * But the effort in expanding the sub-tree is wasted - once we have
      { expr_kv on the stack there are no valid sentences that continue
      with expr_lst, and detecting this would save significant effort.
    * Leaving this tes-case as a reminder that there is an optimization
      opportunity we build the automaton slightly differently - we could
      encode commitment to a particular parse by introducing a distinct
      state after the first reduction of the non-terminal with the repeating
      modifier.
    * This might be a lot of work to analyse properly so skipping it for now
      to continue experimenting with the pidgin grammar.

---
+++ tests/toy_setmaplist/bootstrapper.py
@@ -0,0 +1,59 @@
+# Copyright (C) 2003 Dr Andrew Moss.    You should have received a copy of the GNU General Public License
+#                                       along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os, sys
+rootDir= os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+if rootDir not in sys.path:
+    sys.path.append(rootDir)
+
+import string
+from bootstrap.grammar import Grammar
+
+brackets = ( ("(",")"), ("[","]"), ("{","}"), ("<",">") )
+
+def build():
+    '''
+    Just the part of the pidgin grammar that describes set and map literals with numbers as atoms
+    '''
+    g = Grammar("atom")
+    g.setDiscard(g.Terminal(set(" \t\r\n"), "some"))
+
+    atom = g.addRule("atom", [g.Terminal(set("0123456789"),"some")])
+    atom.add(                [g.Nonterminal("set")])
+    atom.add(                [g.Nonterminal("map")])
+
+    aset = g.addRule("set",   [g.Terminal('{'), g.Nonterminal("expr_lst", "any"), g.Terminal('}')])
+    aord = g.addRule("order", [g.Terminal('['), g.Nonterminal("expr_lst", "any"), g.Terminal(']')])
+    amap = g.addRule("map",   [g.Terminal('{'), g.Nonterminal("expr_kv",  "some"),  g.Terminal('}')])
+    amap.add(                 [g.Terminal('{'), g.Terminal(':'), g.Terminal('}')])
+
+    g.addRule("expr_kv",  [g.Nonterminal("atom"),
+                           g.Terminal(":"),
+                           g.Nonterminal("atom"),
+                           g.Terminal(",", external="optional")])
+    g.addRule("expr_lst", [g.Nonterminal("atom"), g.Terminal(",", external="optional")])
+
+    return g
+
+# The spot for manual testing of the parser
+if __name__=="__main__":
+    grammar = build()
+    from bootstrap.parser import Parser
+    parser = Parser(grammar, discard=grammar.discard)
+    parser.dotAutomaton(open("lr0.dot","wt"))
+    res = (list(parser.parse("{2:3 22 5:4}",trace=open("trace.dot","wt"))))
+    for r in res:
+        r.dump()
+
+# The spot for manual testing of the generator
+#if __name__=="__main__":
+#    import itertools
+#    from bootstrap.generator import Generator
+#    grammar, graph = build()
+#    generator = Generator(grammar)
+#    sentences = list(itertools.islice(generator.step(), 2000))
+#    with open("generated.txt","wt") as outputFile:
+#        for s in sentences:
+#            s = [ symb.string if symb.string is not None else list(symb.chars)[0] for symb in s]
+#            outputFile.write("".join(s))
+#            outputFile.write("\n")
