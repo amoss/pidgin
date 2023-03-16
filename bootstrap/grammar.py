0b98bf2ab2368edb3d84cff7154cf06a4e6c30d0
Andrew Moss <1043609+amoss@users.noreply.github.com>
2023-03-16 11:23:28 +0100

    Updated terminals in grammar.
    * Repeating modifiers are always greedy for terminals - the alternative is a much more
      complex match interface with the parser. If non-greediness is required it can be
      simulated by wrapping in a non-terminal.
    * Split the Grammar.Terminal class into a case for string literals and a case for
      character sets.

--- bootstrap/grammar.py
+++ bootstrap/grammar.py
@@ -106,82 +106,98 @@ class Grammar:
             for clause in rule.clauses:
                 print(f"  {strs(clause.rhs)}")
 
-
-    class Terminal:
-        def __init__(self, match, internal="just", external="just", inverse=False, tag=''):
-            '''The *internal* modifier is applied to matching character classes within the span of
-               text that the Terminal matches. The *external* modifier is used to allow repetitions
-               of the Terminal in the same manner as Nonterminals. Example:
-                 Terminal("x","some") would match "x", "xx" ... -> T("x"), T("xx")...
-                 Terminal("x","just","some") would match "x" -> T("x"), "xx" -> [T("x"),T("x")]...
-               '''
-            assert internal in ["just", "some"], internal
-            assert external in ["any", "just", "some", "optional"], external
-            if isinstance(match, str):
-                self.string = match
-                self.chars  = None
-            else:
-                self.chars  = frozenset(match)
-                self.string = None
-            self.internal = internal
-            self.modifier = external
-            self.inverse  = inverse
-            self.tag      = tag
+    class TermString:
+        def __init__(self, match, modifier="just", tag=''):
+            '''A terminal symbol that matches a string literal *match*. If the *modifier* is repeating then
+               the match is always greedy. If non-greedy matching is required then it can be simulated by
+               wrapping in a non-terminal (with glue if appropriate).'''
+            self.string = match
+            self.tag = tag
+            assert modifier in ("any","just","some","optional"), modifier
+            self.modifier = modifier
 
         def __str__(self):
-            result = f"T({self.internal},{self.modifier},"
             tag = f",{self.tag}" if self.tag!="" else ""
-            if self.string is not None:
-                return result+f"{self.string}{tag})"
-            inv = "inv " if self.inverse else ""
-            if len(self.chars)<5:
-                return result + f" {inv}{self.chars}{tag})"
-            return result + f" {inv}{len(self.chars)} elements{tag})"
+            return f"T({repr(self.string)},{self.modifier}{tag})"
 
-        def order(self):
-            if self.chars is not None:
-                return (0, 0, self.chars)
-            return (0, 1, self.string)
+        def sig(self):
+            return (self.match, self.tag, self.modifier)
 
-        def __eq__(self, other):
-            return isinstance(other,Grammar.Terminal) \
-               and self.string==other.string \
-               and self.chars==other.chars \
-               and self.internal==other.internal \
-               and self.modifier==other.modifier \
-               and self.inverse==other.inverse \
-               and self.tag==other.tag
+        def __eq__(self,other):
+            return self.sig()==other.sig()
 
         def __hash__(self):
-            return hash((self.string, self.chars, self.internal, self.modifier, self.inverse, self.tag))
+            return hash(self.sig())
 
         def match(self, input):
             allowzero = self.internal in ("any","optional")
             limit = len(input) if self.internal in ("any","some") else 1
-            if self.string is not None:
-                i = 0
-                n = len(self.string)
-                original = input[:]
+            i = 0
+            n = len(self.string)
+            original = input[:]
+            #print(f"Matching {i} {n} {self.string} {input}")
+            while i<limit and input[:n]==self.string:
+                i += 1
+                input = input[n:]
                 #print(f"Matching {i} {n} {self.string} {input}")
-                while i<limit and input[:n]==self.string:
-                    i += 1
-                    input = input[n:]
-                    #print(f"Matching {i} {n} {self.string} {input}")
-                if i==0:
-                    return "" if allowzero else None
-                return original[:i*n]
-            if self.chars is not None:
-                i = 0
-                while i<limit and i<len(input) and ((input[i] not in self.chars) == self.inverse):
-                    i += 1
-                if i==0:
-                    return "" if allowzero else None
-                return input[:i]
-            assert False
+            if i==0:
+                return "" if allowzero else None
+            return original[:i*n]
+
+        # Does this still get used or is it dead?
+        #def order(self):
+        #    return (0, 1, self.string)
+
+    class TermSet:
+        def __init__(self, charset, strength="greedy", modifier="just", inverse=False, tag=''):
+            '''The *modifier* applies to matching the *charset* within a single symbol, in contrast
+               to how the modifier works on non-terminals.'''
+            assert strength in ("all","weak","greedy"), strength
+            assert modifier in ("any","just","some","optional"), modifier
+            self.chars = frozenset(charset)
+            self.strength = strength
+            self.modifier = modifier
+            self.inverse  = inverse
+            self.tag      = tag
+
+        def __str__(self):
+            if len(sel.chars)<=5:
+                charset = ",".join([c if c!=',' else "','" for c in self.chars])
+            else:
+                charset = ",".join([c if c!=',' else "','" for c in list(self.chars)[:5]]) + f' +{len(self.chars)-5}'
+            if self.inverse:
+                result = 'T(^{"' + charset + '},'
+            else:
+                result = 'T({"' + charset + '},'
+            tag = f",{self.tag}" if self.tag!="" else ""
+            return f'{result}{strength},{modifier}{tag})'
+
+        def sig(self):
+            return (self.chars, self.strength, self.modifier, self.inverse, self.tag)
+
+        def __eq__(self,other):
+            return self.sig()==other.sig()
+
+        def __hash__(self):
+            return hash(self.sig())
 
         def exactlyOne(self):
-            return Grammar.Terminal(self.string if self.chars is None else self.chars, "just",
-                                    inverse=self.inverse, tag=self.tag)
+            return Grammar.TermSet(self.chars, modifier="just", inverse=self.inverse, tag=self.tag)
+
+        # Does this still get used or is it dead?
+        #def order(self):
+        #    return (0,0,self.chars)
+
+        def match(self, input):
+            allowzero = self.internal in ("any","optional")
+            limit = len(input) if self.internal in ("any","some") else 1
+            i = 0
+            while i<limit and i<len(input) and ((input[i] not in self.chars) == self.inverse):
+                i += 1
+            if i==0:
+                return "" if allowzero else None
+            return input[:i]
+
 
     class Nonterminal:
         def __init__(self, name, modifier="just"):
