1ff6a88ba14ad26dcfd11baad20f16296befc420
Andrew Moss <1043609+amoss@users.noreply.github.com>
2023-06-02 09:52:44 +0200

    Cleaning up
    * Remove all the dead code from execution, replaced by the translation
      into (and execution of) the VM.
    * Mark most of the evalaution code in box as dead. Will be removed once
      the other operations are moved into the VM.

--- bootstrap/interpreter/box.py
+++ bootstrap/interpreter/box.py
@@ -24,11 +24,11 @@ class Box:
     def __str__(self):
         return f'box({self.type},{self.raw})'
 
-    def plusTypeCheck(left, right):
-        if not left.eqOrCoerce(right): return None
-        if left.label=='{}':
-            return lambda l,r: Box(l.type.join(r.type),l.raw | r.raw)
-        return lambda l,r: Box(l.type.join(r.type),l.raw+r.raw)
+#    def plusTypeCheck(left, right):
+#        if not left.eqOrCoerce(right): return None
+#        if left.label=='{}':
+#            return lambda l,r: Box(l.type.join(r.type),l.raw | r.raw)
+#        return lambda l,r: Box(l.type.join(r.type),l.raw+r.raw)
 
     def postfixTypeCheck(left, right):
         if not left.eqOrCoerce(right): return None
@@ -79,72 +79,72 @@ class Box:
         if left.label=='S' and right.label=='S':
             return lambda whole,delimiter: Box(Type('[]',param1=Type('S')), [Box(Type('S'),s) for s in whole.raw.split(delimiter.raw)])
 
-    opTypeCheck = {
-        '+':  plusTypeCheck,
-        '.+': postfixTypeCheck,
-        '+.': prefixTypeCheck,
-        '-':  subTypeCheck,
-        '.-': postdropTypeCheck,
-        '-.': predropTypeCheck,
-        '*':  starTypeCheck,
-        '/':  slashTypeCheck
-    }
+#    opTypeCheck = {
+#        '+':  plusTypeCheck,
+#        '.+': postfixTypeCheck,
+#        '+.': prefixTypeCheck,
+#        '-':  subTypeCheck,
+#        '.-': postdropTypeCheck,
+#        '-.': predropTypeCheck,
+#        '*':  starTypeCheck,
+#        '/':  slashTypeCheck
+#    }
 
-    @staticmethod
-    def evaluateBinop(tree, listTag, env):
-        accumulator = evaluate(tree.children[0], env)
-        for c in tree.children[1:]:
-            assert isinstance(c, Token) and c.symbol.isNonterminal and c.tag==listTag and len(c.children)==2, c
-            assert isinstance(c.children[0], Token) and c.children[0].symbol.isTerminal, c.children[0]
-            value = evaluate(c.children[1], env)
-            op = c.children[0].span
-            print(f'evalBinop lhs={accumulator} op={op} rhs={value}')
-            eval = Box.opTypeCheck[op](accumulator.type, value.type)
-            assert eval is not None, f"Invalid types for operation: {accumulator.type} {op} {value.type}"
-            accumulator = eval(accumulator, value)
-        return accumulator
+#    @staticmethod
+#    def evaluateBinop(tree, listTag, env):
+#        accumulator = evaluate(tree.children[0], env)
+#        for c in tree.children[1:]:
+#            assert isinstance(c, Token) and c.symbol.isNonterminal and c.tag==listTag and len(c.children)==2, c
+#            assert isinstance(c.children[0], Token) and c.children[0].symbol.isTerminal, c.children[0]
+#            value = evaluate(c.children[1], env)
+#            op = c.children[0].span
+#            print(f'evalBinop lhs={accumulator} op={op} rhs={value}')
+#            eval = Box.opTypeCheck[op](accumulator.type, value.type)
+#            assert eval is not None, f"Invalid types for operation: {accumulator.type} {op} {value.type}"
+#            accumulator = eval(accumulator, value)
+#        return accumulator
 
-    @staticmethod
-    def evaluateBinop1(tree, env):
-        return Box.evaluateBinop(tree, "binop1_lst", env)
-
-    @staticmethod
-    def evaluateBinop2(tree, env):
-        return Box.evaluateBinop(tree, "binop2_lst", env)
-
-    @staticmethod
-    def evaluateOrder(tree, env):
-        if len(tree.seq)==0:  return Box(Type('[]',param1='empty'),[])
-        result = [ evaluate(tree.seq[0], env) ]
-        for subtree in tree.seq[1:]:
-            element = evaluate(subtree,env)
-            assert result[0].type.eqOrCoerce(element.type), f"Can't store element {element.type} inside [{result[0].type}]!"
-            result.append(element)
-        return Box(Type('[]', param1=result[0].type), result)
-
-    @staticmethod
-    def evaluateSet(tree, env):
-        if len(tree.elements)==0: return Box(Type('{}',param1='empty'),frozenset())
-        element = evaluate(tree.elements[0], env)
-        elementType = element.type
-        result = set([element])
-        for subtree in tree.elements[1:]:
-            element = evaluate(subtree, env)
-            assert elementType.eqOrCoerce(element.type), f"Can't store element {element.type} inside \{{elementType}}!"
-            result.add(element)
-        return Box(Type('{}', param1=elementType), frozenset(result))
-
-
-    @staticmethod
-    def evaluateRecord(tree, env):
-        result = Box(Type('[:]'), Environment())
-        dump(tree)
-        for child in tree.children:
-            assert isinstance(child, AST.IdentVal), child
-            assert not result.raw.contains(child.key), child.key
-            value = evaluate(child.value, env)
-            result.raw.insert(child.key, value.type, value)
-        return result
+#    @staticmethod
+#    def evaluateBinop1(tree, env):
+#        return Box.evaluateBinop(tree, "binop1_lst", env)
+#
+#    @staticmethod
+#    def evaluateBinop2(tree, env):
+#        return Box.evaluateBinop(tree, "binop2_lst", env)
+#
+#    @staticmethod
+#    def evaluateOrder(tree, env):
+#        if len(tree.seq)==0:  return Box(Type('[]',param1='empty'),[])
+#        result = [ evaluate(tree.seq[0], env) ]
+#        for subtree in tree.seq[1:]:
+#            element = evaluate(subtree,env)
+#            assert result[0].type.eqOrCoerce(element.type), f"Can't store element {element.type} inside [{result[0].type}]!"
+#            result.append(element)
+#        return Box(Type('[]', param1=result[0].type), result)
+#
+#    @staticmethod
+#    def evaluateSet(tree, env):
+#        if len(tree.elements)==0: return Box(Type('{}',param1='empty'),frozenset())
+#        element = evaluate(tree.elements[0], env)
+#        elementType = element.type
+#        result = set([element])
+#        for subtree in tree.elements[1:]:
+#            element = evaluate(subtree, env)
+#            assert elementType.eqOrCoerce(element.type), f"Can't store element {element.type} inside \{{elementType}}!"
+#            result.add(element)
+#        return Box(Type('{}', param1=elementType), frozenset(result))
+#
+#
+#    @staticmethod
+#    def evaluateRecord(tree, env):
+#        result = Box(Type('[:]'), Environment())
+#        dump(tree)
+#        for child in tree.children:
+#            assert isinstance(child, AST.IdentVal), child
+#            assert not result.raw.contains(child.key), child.key
+#            value = evaluate(child.value, env)
+#            result.raw.insert(child.key, value.type, value)
+#        return result
 
 
     def unbox(self):
@@ -156,4 +156,3 @@ class Box:
             raw = self.raw
         return raw
 
-from .execution import evaluate #, Environment
