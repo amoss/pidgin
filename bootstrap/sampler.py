abf0603ee07c7641cb309e32d7c031f097ba9a23
Andrew Moss <1043609+amoss@users.noreply.github.com>
2023-07-29 10:48:58 +0200

    Basic structure of an enumerator for grammars
    * This is a bit awkward - the modifiers on symbols make writing grammars
      easier, and while they do not impact the difficulty of parsing (too much)
      they make the code for enumeration very awkward.
    * Because each symbol can potentially repeat we need to construct an
      alignment of how instantiations of the symbols for different target sizes
      will appear within the result.
    * We can then step through the combinations of sizes (as in the counting
      function) and project the combination onto a set of symbol-strings that
      appear at a known position within the result alignment.
    * This alignment is then collated onto the result.
    * The code is a bit bloated for the problem, but need to put the nonterminal
      instantiations in before reducing it...

--- bootstrap/sampler.py
+++ bootstrap/sampler.py
@@ -6,10 +6,12 @@ rootDir= os.path.dirname(os.path.dirname(__file__))
 if rootDir not in sys.path:
     sys.path.append(rootDir)
 
+import itertools
 import math
 import random
 from bootstrap.grammar import Grammar
 from bootstrap.interpreter import buildCommon, stage2
+from bootstrap.util import strs
 
 
 def ordered_partitions_n(total, length):
@@ -109,9 +111,60 @@ class Sampler:
         return combinations
 
 
+class Enumerator:
+    def __init__(self, grammar):
+        self.grammar = grammar
+        self.memo    = {}
+
+    def produce(self, ruleName, size):
+        rule = self.grammar.rules[ruleName]
+        for clause in rule.clauses:
+            for solution in self.produce_clause(clause,size):
+                yield solution
+
+    def produce_clause(self, clause, size):
+        resultAlignment = [s for s in clause.rhs      if s.isTerminal or s.isNonterminal]
+        terminals       = [s for s in resultAlignment if s.isTerminal ]
+        nonTerms        = [s for s in resultAlignment if s.isNonterminal ]
+        exact, zeroOne, zeroMore, oneMore = [], [], [], []
+        modifierMap = { "just":exact, "optional":zeroOne, "any":zeroMore, "some":oneMore }
+        for i,s in enumerate(resultAlignment):
+            if s.isTerminal:
+                modifierMap[s.modifier].append((i,s))
+        def p(col):
+            return ", ".join([str(c[1]) + '@' + str(c[0]) for c in col])
+        print(f'{size} of exact {p(exact)} 01 {p(zeroOne)} 0+ {p(zeroMore)} 1+ {p(oneMore)}')
+        freeTerms = size - len(exact) - len(oneMore)
+        flexible = len(zeroMore) + len(oneMore)
+        if freeTerms == 0:
+            result = [None] * len(resultAlignment)
+            for (pos,s) in exact+oneMore:
+                result[pos] = s
+            yield [s for s in result if s is not None]
+        elif freeTerms>0 and flexible>0:
+            for optSizes in ordered_binary_partitions_below_n(freeTerms, len(zeroOne)):
+                usedTerms = sum(optSizes)
+                stillFree = freeTerms - usedTerms
+                for subsizes in ordered_partitions_n(stillFree, flexible):
+                    print(f'opt: {optSizes} flex: {subsizes}')
+                    result = [(),] * len(resultAlignment)
+                    for (pos,s) in exact+oneMore:
+                        result[pos] = [s]
+                    for f in range(len(zeroMore)):
+                        pos,symbol = zeroMore[f]
+                        result[pos] = [symbol] * subsizes[f]
+                    yield list(itertools.chain.from_iterable([s for s in result if len(s)>0]))
+
+
+
+
+
+
 if __name__=='__main__':
     stage1g, _, _ = buildCommon()
-    s = Sampler(stage1g)
-    for i in range(1,5):
-        print(s.count_rule('map',i))
+    #s = Sampler(stage1g)
+    e = Enumerator(stage1g)
+    for i in range(0,5):
+        for r in e.produce('str_lit2',i):
+            print(f'Solution: {strs(r)}')
 
