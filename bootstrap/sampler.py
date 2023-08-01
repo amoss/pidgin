d2d8a49f40e43c4201e4500d867e091fc219ac77
Andrew Moss <1043609+amoss@users.noreply.github.com>
2023-08-01 18:27:29 +0200

    Split out the common structure from the enumerator.
    * The results look correct, but there must be a simple bug that the test data does
      not cover.
    * When we took the part of the assignment for 1+ terminals in the old enumerator
      the code manually adds one to the count, there is a corresponding reduction in the
      number of free terms to match (i.e. we count one terminal from each of the 1+
      terminal productions as part of the fixed size).
    * The new code does not add one to the counts for 1+ terminal productions yet the
      enumerated results look correct. Need to tweak the test data slightly...

--- bootstrap/sampler.py
+++ bootstrap/sampler.py
@@ -65,6 +65,7 @@ class ClauseAllocation:
                 self.zero.append( (i,symbol) )
         self.numFlexible = len(self.zeroMore) + len(self.oneMore) + len(self.nonterms)
         self.posNonterms = len(self.zeroOne) + len(self.zeroMore) + len(self.oneMore)
+        self.assignmentOrder = self.zeroOne + self.zeroMore + self.oneMore + self.nonterms
 
     def assignments(self, size):
         freeTerms = size - len(self.one) - len(self.oneMore)
@@ -74,6 +75,7 @@ class ClauseAllocation:
             for optSizes in ordered_binary_partitions_below_n(freeTerms, len(self.zeroOne)):
                 stillFree = freeTerms - sum(optSizes)
                 for subsizes in ordered_partitions_n(stillFree, self.numFlexible):
+                    # insert +1 for oneMore here
                     yield optSizes+subsizes
 
     def assignment_nonterms(self, assignment):
@@ -126,9 +128,9 @@ class Sampler:
         alloc = ClauseAllocation(clause.rhs)
 
         combinations = 0
-        for coefficients in alloc.assignments(size):
+        for counts in alloc.assignments(size):
             combinations += math.prod(self.count_nonterminal(nonterm,subsize)
-                                      for subsize,nonterm in alloc.assignment_nonterms(coefficients))
+                                      for subsize,nonterm in alloc.assignment_nonterms(counts))
         return combinations
 
 
@@ -178,51 +180,68 @@ class Enumerator:
                 yield solution
 
     def produce_clause(self, clause, size):
-        terminals       = [s for s in clause.rhs if s.isTerminal ]
-        exact, zeroOne, zeroMore, oneMore, subTerms = [], [], [], [], []
-        modifierMap = { "just":exact, "optional":zeroOne, "any":zeroMore, "some":oneMore }
-        for i,s in enumerate(clause.rhs):
-            if s.isTerminal:
-                modifierMap[s.modifier].append((i,s))
-            if s.isNonterminal:
-                subTerms.append((i,s))
-        def p(col):
-            return ", ".join([str(c[1]) + '@' + str(c[0]) for c in col])
-        #print(f'{size} of exact {p(exact)} 01 {p(zeroOne)} 0+ {p(zeroMore)} 1+ {p(oneMore)} nt {p(subTerms)}')
-        freeTerms = size - len(exact) - len(oneMore)
-        flexible = len(zeroMore) + len(oneMore) + len(subTerms)
-        if freeTerms==0 and len(exact)+len(oneMore)>0 and \
-           all([] in self.nonterm_expansion(s,0) for (_,s) in subTerms):
-            result = [None] * len(clause.rhs)
-            for (pos,s) in exact+oneMore:
-                result[pos] = s
-            for i,s in enumerate(clause.rhs):
-                if not s.isTerminal and not s.isNonterminal:
-                    result[i] = clause.rhs[i]
-            yield [s for s in result if s is not None]
-        elif freeTerms>0 and flexible>0:
-            for optSizes in ordered_binary_partitions_below_n(freeTerms, len(zeroOne)):
-                usedTerms = sum(optSizes)
-                stillFree = freeTerms - usedTerms
-                for subsizes in ordered_partitions_n(stillFree, flexible):
-                    #print(f' opt: {optSizes} flex: {subsizes}')
-                    result = [(),] * len(clause.rhs)
-                    for i,s in enumerate(clause.rhs):
-                        if not s.isTerminal and not s.isNonterminal:
-                            result[i] = [clause.rhs[i]]
-                    for (pos,s) in exact+oneMore:
-                        result[pos] = [s]
-                    for f in range(len(zeroMore)):
-                        pos,symbol = zeroMore[f]
-                        result[pos] = [symbol] * subsizes[f]
-                    for f in range(len(zeroMore),len(zeroMore)+len(oneMore)):
-                        pos,symbol = oneMore[f-len(zeroMore)]
-                        result[pos] = [symbol] * (subsizes[f]+1)
-                    for subSolution in self.nonterm_seq_expansion(subsizes[len(zeroMore)+len(oneMore):], subTerms):
-                        for pos,terms in subSolution:
-                            result[pos] = terms
-                        #print(f' raw: {result}')
-                        yield list(itertools.chain.from_iterable([s for s in result if len(s)>0]))
+        alloc = ClauseAllocation(clause.rhs)
+
+        for counts in alloc.assignments(size):
+            result = [(),] * len(clause.rhs)
+            for pos,symbol in alloc.zero+alloc.one:
+                result[pos] = [symbol]
+            combined = zip(counts, alloc.assignmentOrder)
+            for count,(pos,symbol) in combined:
+                result[pos] = [symbol]*count
+            for subSolution in self.nonterm_seq_expansion(counts[alloc.posNonterms:],
+                                                          alloc.assignmentOrder[alloc.posNonterms:]):
+                for pos,terms in subSolution:
+                    result[pos] = terms
+                yield list(itertools.chain.from_iterable([s for s in result if len(s)>0]))
+
+
+#    def produce_clause(self, clause, size):
+#        terminals       = [s for s in clause.rhs if s.isTerminal ]
+#        exact, zeroOne, zeroMore, oneMore, subTerms = [], [], [], [], []
+#        modifierMap = { "just":exact, "optional":zeroOne, "any":zeroMore, "some":oneMore }
+#        for i,s in enumerate(clause.rhs):
+#            if s.isTerminal:
+#                modifierMap[s.modifier].append((i,s))
+#            if s.isNonterminal:
+#                subTerms.append((i,s))
+#        def p(col):
+#            return ", ".join([str(c[1]) + '@' + str(c[0]) for c in col])
+#        #print(f'{size} of exact {p(exact)} 01 {p(zeroOne)} 0+ {p(zeroMore)} 1+ {p(oneMore)} nt {p(subTerms)}')
+#        freeTerms = size - len(exact) - len(oneMore)
+#        flexible = len(zeroMore) + len(oneMore) + len(subTerms)
+#        if freeTerms==0 and len(exact)+len(oneMore)>0 and \
+#           all([] in self.nonterm_expansion(s,0) for (_,s) in subTerms):
+#            result = [None] * len(clause.rhs)
+#            for (pos,s) in exact+oneMore:
+#                result[pos] = s
+#            for i,s in enumerate(clause.rhs):
+#                if not s.isTerminal and not s.isNonterminal:
+#                    result[i] = clause.rhs[i]
+#            yield [s for s in result if s is not None]
+#        elif freeTerms>0 and flexible>0:
+#            for optSizes in ordered_binary_partitions_below_n(freeTerms, len(zeroOne)):
+#                usedTerms = sum(optSizes)
+#                stillFree = freeTerms - usedTerms
+#                for subsizes in ordered_partitions_n(stillFree, flexible):
+#                    #print(f' opt: {optSizes} flex: {subsizes}')
+#                    result = [(),] * len(clause.rhs)
+#                    for i,s in enumerate(clause.rhs):
+#                        if not s.isTerminal and not s.isNonterminal:
+#                            result[i] = [clause.rhs[i]]
+#                    for (pos,s) in exact+oneMore:
+#                        result[pos] = [s]
+#                    for f in range(len(zeroMore)):
+#                        pos,symbol = zeroMore[f]
+#                        result[pos] = [symbol] * subsizes[f]
+#                    for f in range(len(zeroMore),len(zeroMore)+len(oneMore)):
+#                        pos,symbol = oneMore[f-len(zeroMore)]
+#                        result[pos] = [symbol] * (subsizes[f]+1)
+#                    for subSolution in self.nonterm_seq_expansion(subsizes[len(zeroMore)+len(oneMore):], subTerms):
+#                        for pos,terms in subSolution:
+#                            result[pos] = terms
+#                        #print(f' raw: {result}')
+#                        yield list(itertools.chain.from_iterable([s for s in result if len(s)>0]))
 
     def nonterm_seq_expansion(self, sizes, nonterms):
         #print(f' seq_exp: {strs(sizes)} {nonterms}')
@@ -285,9 +304,9 @@ if __name__=='__main__':
     stage1g, _, _ = buildCommon()
     s = Sampler(stage1g)
     e = Enumerator(stage1g)
-    for i in range(10):
-        enumerated = [renderText(r) for r in e.produce('atom',i)]
-        counted = s.count_rule('atom',i)
+    for i in range(4,5):
+        enumerated = [renderText(r) for r in e.produce('set',i)]
+        counted = s.count_rule('set',i)
         print(f'count: {counted}  enum: {len(enumerated)}')
-        #print(enumerated)
+        print(enumerated)
 
